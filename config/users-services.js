94765a7c68ed2789ee852955a8bed6e27167dbc9
Richard Greene <rhgreene110@mac.com>
2019-05-21 20:02:29 -0500

    initial files

---
+++ config/users-services.js
@@ -0,0 +1,132 @@
+var Hero = require('./database');
+ 
+exports.create = (req, res) => {
+    const hero = new Hero({
+        first_name: req.body.first_name || 'Unknown name',
+        middle_initial: req.body.middle_initial || 'N/A',
+        last_name: req.body.last_name || 'N/A',
+        street_address: req.body.street_address || 'N/A',
+        city_name: req.body.city_name || 'N/A',
+        payment_card: req.body.payment_card || 'N/A',
+        country_name: req.body.country_name || 'N/A',
+        day_of_birth: req.body.day_of_birth || '',
+        state_name: req.body.state_name || '',
+       
+        username: req.body.username || 'Undefined',
+        password: req.body.password || '',
+        email: req.body.email || 'Undefined',
+    });
+ 
+    hero.save()
+        .then(data=>{
+            console.log(data);
+            res.redirect('/users');
+        })
+        .catch(err =>{
+            res.status(500).send("failed to create"+err);
+        })
+};
+ 
+exports.findAll = (req, res) => {
+    Hero.find()
+        .then(data =>{
+            // res.send(data);
+            console.log('findAll success');
+            res.render('Users', { layout: 'layout', usersData: data});
+        })
+        .catch(err=>{
+            res.status(500).send('failed to findall' + err);
+        })
+};
+ 
+exports.findOne = (req, res) => {
+    Hero.findById(req.params.Id)
+    .then(data=>{
+        if(!data){
+            return res.statue(404).send('record not found' + req.params.Id);
+        }
+        console.log('findOne success');
+        res.send(data);
+    })
+    .catch(err=>{
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send('record not found' + req.params.Id);
+        }
+        return res.status(500).send('error updating with Id' + req.params.Id)
+    });
+};
+ 
+exports.update = (req, res) => {
+    Hero.findByIdAndUpdate(req.params.Id, {
+        first_name: req.body.first_name || 'Unknown name',
+        middle_initial: req.body.middle_initial || 'N/A',
+        last_name: req.body.last_name || 'N/A',
+        street_address: req.body.street_address || 'N/A',
+        city_name: req.body.city_name || 'N/A',
+        payment_card: req.body.payment_card || 'N/A',
+        country_name: req.body.country_name || 'N/A',
+        day_of_birth: req.body.day_of_birth || '',
+        state_name: req.body.state_name || '',
+        username: req.body.username || 'Undefined',
+        password: req.body.password || '',
+        email: req.body.email || 'Undefined',
+    }, {new: true})
+    .then(data => {
+        if(!data){
+            return res.status(404).send('record not found' + req.params.Id);
+        }
+        console.log('update success');
+        return res.redirect('/users');
+    })
+    .catch(err=>{
+        if(err.kind === 'ObjectId'){
+            console.log('record not found' + req.params.Id);
+            return res.status(404).send('record not found' + req.params.Id);
+        }
+        return res.status(500).send('error updating record with Id' + req.params.Id)
+    });
+};
+ 
+exports.delete = (req, res) => {
+    Hero.findByIdAndRemove(req.params.Id)
+    .then(data=>{
+        if(!data){
+            return res.status(404).send('record not found' + req.params.Id);
+        }
+        console.log('delete success');
+        return res.redirect('/users');
+    })
+    .catch(err=>{
+        if(err.kind === 'ObjectId' || err.name === 'NotFound'){
+            console.log('record not found' + req.params.Id);
+            return res.status(404).send('record not found' + req.params.Id);
+        }
+        return res.status(500).send('error deleting record with Id' + req.params.Id)
+    });
+};
+exports.userregister = (req, res) => {
+ 
+    const hero = new Hero({
+        first_name: req.body.first_name || 'Unknown name',
+        middle_initial: req.body.middle_initial || 'N/A',
+        last_name: req.body.last_name || 'N/A',
+        street_address: req.body.street_address || 'N/A',
+        city_name: req.body.city_name || 'N/A',
+        payment_card: req.body.payment_card || 'N/A',
+        country_name: req.body.country_name || 'N/A',
+        day_of_birth: req.body.day_of_birth || '',
+        state_name: req.body.state_name || '',       
+        username: req.body.username || 'Undefined',
+        password: req.body.password || '',
+        email: req.body.email || 'Undefined',
+    });
+ 
+    hero.save()
+        .then(data=>{
+            // console.log(data);
+            res.redirect('/');
+        })
+        .catch(err =>{
+            res.status(500).send("failed to register"+err);
+        })
+};
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var router = express.Router();
+ 
+const userServices  = require('../config/users-services');
+ 
+/* GET users listing. */
+router.post('/', userServices.create);
+ 
+router.get('/', userServices.findAll);
+ 
+router.get('/:Id', userServices.findOne);
+ 
+router.post('/edit/:Id', userServices.update);
+ 
+router.post('/delete/:Id', userServices.delete);
+ 
+router.post('/register', userServices.userregister);
+
+module.exports = router;
